7cc1490ecfb41421350d37bab8dfb0e0901c14d0
Wedge129 <wedge0129@gmail.com>
2023-10-11 08:25:02 -0400

    Added Hover Effect

---
+++ calculator.js
@@ -0,0 +1,7 @@
+document.getElementById("resetButton").addEventListener("click",function(){
+console.log("ResetButtonClicked");
+});
+
+document.getElementById("deleteButton").addEventListener("click",function(){
+    console.log("DeleteButtonCLicked");
+    });
